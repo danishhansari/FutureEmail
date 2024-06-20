84f6fe4b41be2e5d8e251e19babd37af8e48f25d
Danish <dan71ish@gmail.com>
2024-06-20 15:04:33 +0530

    ..

--- frontend/src/components/BlogCarousel.tsx
+++ frontend/src/components/BlogCarousel.tsx
@@ -8,12 +8,12 @@ import { blogdata } from "@/data";
 
 export const BlogCarousel = () => {
   return (
-    <>
+    <div className="max-w-4xl mx-auto">
       <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Need some inspiration?
       </h2>
 
-      <Card className="max-w-xs border-none">
+      <Card className="border-none bg-foreground">
         <div className="md:hidden">
           <Carousel className="bg-foreground">
             <CarouselContent>
@@ -39,7 +39,25 @@ export const BlogCarousel = () => {
             </CarouselContent>
           </Carousel>
         </div>
+
+        <div className="hidden md:grid grid-cols-2 max-w-4xl mx-auto py-4 gap-8 ">
+          {blogdata.map((blog) => {
+            return (
+              <Card className="bg-foreground border-none" key={blog.id}>
+                <img
+                  src={blog.img}
+                  alt="Cover image"
+                  className="w-full rounded-t-lg"
+                />
+                <CardTitle className="font-mono text-sm font-medium my-1 text-background">
+                  {blog.category}
+                </CardTitle>
+                <CardTitle className="text-background">{blog.title}</CardTitle>
+              </Card>
+            );
+          })}
+        </div>
       </Card>
-    </>
+    </div>
   );
 };
