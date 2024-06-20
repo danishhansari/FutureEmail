7b07a7b7e8a7da51bbc89672e1d85aaab61b335a
Danish <dan71ish@gmail.com>
2024-06-20 14:35:31 +0530

    fix carousel in small device

--- frontend/src/components/BlogCarousel.tsx
+++ frontend/src/components/BlogCarousel.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardTitle } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -8,37 +8,34 @@ import { blogdata } from "@/data";
 
 export const BlogCarousel = () => {
   return (
-    <div className="max-w-lg mx-auto">
+    <div>
       <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Need some inspiration?
       </h2>
 
-      <Card className="w-full max-w-md mx-auto border-none">
-        <div className="grid gap-4">
-          <div className="md:hidden">
-            <Carousel className="w-full bg-foreground">
-              <CarouselContent>
-                {blogdata.map((blog) => {
-                  return (
-                    <CarouselItem
-                      key={blog.id}
-                      className="bg-foreground text-background"
-                    >
-                      <img
-                        src={blog.img}
-                        alt="Cover image"
-                        className="aspect-video object-cover rounded-t-lg"
-                      />
-                      <CardHeader>
-                        <CardTitle>{blog.category}</CardTitle>
-                        <CardTitle>{blog.title}</CardTitle>
-                      </CardHeader>
-                    </CarouselItem>
-                  );
-                })}
-              </CarouselContent>
-            </Carousel>
-          </div>
+      <Card className="max-w-xs border-none">
+        <div className="md:hidden">
+          <Carousel className="bg-foreground">
+            <CarouselContent>
+              {blogdata.map((blog) => {
+                return (
+                  <CarouselItem
+                    key={blog.id}
+                    className="bg-foreground text-background"
+                  >
+                    <img
+                      src={blog.img}
+                      alt="Cover image"
+                      className="max-w-full rounded-t-lg"
+                    />
+                   
+                    <CardTitle className="font-mono text-sm font-medium my-1 text-background">{blog.category}</CardTitle>
+                    <CardTitle>{blog.title}</CardTitle>
+                  </CarouselItem>
+                );
+              })}
+            </CarouselContent>
+          </Carousel>
           <div className="hidden md:grid md:grid-cols-2 gap-4">
             <img
               src="https://a.storyblok.com/f/127416/1107x603/2be93524b6/header_capture_memory.png"
