270d45cbdafdf0d268722bbf47002afa9b8b2a08
Danish <dan71ish@gmail.com>
2024-08-18 18:25:41 +0530

    fix declarion

--- backend/src/bun.d.ts
+++ backend/src/bun.d.ts
@@ -1,3 +1,7 @@
 declare const Bun: {
   env: { [key: string]: string | undefined };
+  fetch: (request: Request) => Promise<Response>;
+  serve: (options: {
+    fetch: (request: Request) => Response | Promise<Response>;
+  }) => void;
 };
